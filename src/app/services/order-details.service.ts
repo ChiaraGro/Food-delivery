9ace2c4b8b876ba39cb60b0b7def4512d5e5f191
Chiara Grottini <chiaragrottini@gmail.com>
2021-11-21 23:59:54 +0100

    fixed style

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -8,45 +8,45 @@ export class OrderDetailsService {
   foodDetails = [
     {
       id:1,
-      foodName:"Paneer Grilled Sandwich",
-      foodDetails:"Pan-fried masala paneer.",
-      foodPrice:200,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq"
+      foodName:"Salmon Bowl",
+      foodDetails:"Poke a base di salmone, avocado, emadame e carote",
+      foodPrice:12,
+      foodImg:"https://images.unsplash.com/photo-1597958792579-bd3517df6399?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJvd2wlMjBhdm9jYWRvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     },
     {
       id:2,
-      foodName:"Veggie Supreme",
-      foodDetails:"Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
-      foodPrice:369,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
+      foodName:"Pizza Margherita",
+      foodDetails:"Pomodoro, mozzarella, basilico",
+      foodPrice:6,
+      foodImg:"https://images.unsplash.com/photo-1604068549290-dea0e4a305ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
     },
     {
       id:3,
-      foodName:"Paneer Burger",
-      foodDetails:"panner",
-      foodPrice:149,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
+      foodName:"BBQ Burger",
+      foodDetails:"Doppio burger, cipolle, pomodori cetriolini, insalata e la nostra salsa",
+      foodPrice:7.5,
+      foodImg:"https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     },
     {
       id:4,
-      foodName:"Veg Masala Roll In Naan",
+      foodName:"Tiramisu",
       foodDetails:"A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
-      foodPrice:140,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
+      foodPrice:6.5,
+      foodImg:"https://images.unsplash.com/photo-1586040140378-b5634cb4c8fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dGlyYW1pc3V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
     },
     {
       id:5,
       foodName:"Indulgence Brownie",
       foodDetails:"(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.",
-      foodPrice:105,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv"
+      foodPrice:4,
+      foodImg:"https://images.unsplash.com/photo-1630384057168-b537be58939c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnJvd25pZXxlbnwwfHwwfGJsYWNrfA%3D%3D&auto=format&fit=crop&w=500&q=60"
     },
     {
       id:6,
-      foodName:"Oreo Cheesecake Ice Cream",
+      foodName:"Cheescake",
       foodDetails:"Oreo ice cream",
-      foodPrice:219,
-      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6"
+      foodPrice:5.5,
+      foodImg:"https://images.unsplash.com/photo-1524351199678-941a58a3df50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hlZXNlY2FrZXxlbnwwfHwwfGJsYWNrfA%3D%3D&auto=format&fit=crop&w=500&q=60"
     }
   ]
 
